df23c9a3dd295d97549a72e5cca5dd41cd87be6c
Peter Kowalsky <acheloos@live.at>
2014-12-21 03:09:14 +0100

    First.
    Wow.

---
+++ Main.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.Open = New System.Windows.Forms.Button()
+        Me.Start = New System.Windows.Forms.Button()
+        Me.Save = New System.Windows.Forms.Button()
+        Me.OpenT = New System.Windows.Forms.TextBox()
+        Me.SaveT = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Open
+        '
+        Me.Open.Location = New System.Drawing.Point(12, 61)
+        Me.Open.Name = "Open"
+        Me.Open.Size = New System.Drawing.Size(75, 23)
+        Me.Open.TabIndex = 0
+        Me.Open.Text = "Open"
+        Me.Open.UseVisualStyleBackColor = True
+        '
+        'Start
+        '
+        Me.Start.Location = New System.Drawing.Point(12, 127)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(75, 23)
+        Me.Start.TabIndex = 1
+        Me.Start.Text = "Start"
+        Me.Start.UseVisualStyleBackColor = True
+        '
+        'Save
+        '
+        Me.Save.Location = New System.Drawing.Point(12, 90)
+        Me.Save.Name = "Save"
+        Me.Save.Size = New System.Drawing.Size(75, 23)
+        Me.Save.TabIndex = 2
+        Me.Save.Text = "Save to"
+        Me.Save.UseVisualStyleBackColor = True
+        '
+        'OpenT
+        '
+        Me.OpenT.Location = New System.Drawing.Point(95, 64)
+        Me.OpenT.Name = "OpenT"
+        Me.OpenT.Size = New System.Drawing.Size(327, 20)
+        Me.OpenT.TabIndex = 3
+        '
+        'SaveT
+        '
+        Me.SaveT.Location = New System.Drawing.Point(95, 93)
+        Me.SaveT.Name = "SaveT"
+        Me.SaveT.Size = New System.Drawing.Size(327, 20)
+        Me.SaveT.TabIndex = 4
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(95, 127)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(327, 23)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Windows Command Prompt", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(7, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(162, 28)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "FuzzyBlack"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Windows Command Prompt", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(175, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(167, 11)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "By Peter Kowalsky (Acheloos)"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(434, 162)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.SaveT)
+        Me.Controls.Add(Me.OpenT)
+        Me.Controls.Add(Me.Save)
+        Me.Controls.Add(Me.Start)
+        Me.Controls.Add(Me.Open)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(450, 200)
+        Me.MinimumSize = New System.Drawing.Size(450, 200)
+        Me.Name = "Main"
+        Me.Text = "FuzzyBlack"
+        Me.TransparencyKey = System.Drawing.Color.Transparent
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Open As System.Windows.Forms.Button
+    Friend WithEvents Start As System.Windows.Forms.Button
+    Friend WithEvents Save As System.Windows.Forms.Button
+    Friend WithEvents OpenT As System.Windows.Forms.TextBox
+    Friend WithEvents SaveT As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Main.vb
@@ -0,0 +1,82 @@
+﻿Public Class Main
+    Dim fromFileName As String
+    Dim saveFileName As String
+
+    Private Sub Open_Click(sender As System.Object, e As System.EventArgs) Handles Open.Click
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+
+        fd.Title = "Open"
+        fd.Filter = "PNG|*.png|JPG|*.jpg|GIF|*.gif|ICON|*.ico|All files|*.*"
+        fd.FilterIndex = 5
+        fd.RestoreDirectory = True
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            fromFileName = fd.FileName
+        End If
+
+        OpenT.Text = fromFileName
+    End Sub
+
+    Private Sub Start_Click(sender As System.Object, e As System.EventArgs) Handles Start.Click
+        Try
+            Dim img As Bitmap
+            Dim sFileName As String = fromFileName
+            Dim fs As New System.IO.FileStream(sFileName, System.IO.FileMode.Open)
+            img = Bitmap.FromStream(fs)
+            fs.Close()
+            Dim Imgs As New Bitmap(img.Width, img.Height)
+            Dim y As Integer = 0
+            Dim x As Integer = 0
+            ProgressBar1.Minimum = 0
+            ProgressBar1.Maximum = img.Width
+            While x < img.Width
+                While y < img.Height
+                    Dim inputs As String = Microsoft.VisualBasic.Hex(img.GetPixel(x, y).ToArgb)
+                    Dim parts As New List(Of String)
+                    For i As Integer = 0 To inputs.Length - 1 Step 2
+                        parts.Add(inputs.Substring(i, IIf(i + 2 < inputs.Length, 2, inputs.Length - i)))
+                    Next
+                    Dim r As Integer = Convert.ToInt32(parts(1), 16)
+                    Dim g As Integer = Convert.ToInt32(parts(2), 16)
+                    Dim b As Integer = Convert.ToInt32(parts(3), 16)
+                    Dim pix As Integer
+                    pix = (r + g + b) / 3
+                    Imgs.SetPixel(x, y, Color.FromArgb(255, pix, pix, pix))
+                    y = y + 1
+                End While
+                y = 0
+                x = x + 1
+                ProgressBar1.Value = x
+            End While
+            img.Dispose()
+            Imgs.Save(saveFileName, Imaging.ImageFormat.Png)
+        Catch ex As Exception
+            '' Debug here :P
+        End Try
+    End Sub
+
+    Private Sub Save_Click(sender As System.Object, e As System.EventArgs) Handles Save.Click
+
+        Dim fs As New SaveFileDialog()
+        fs.Filter = "PNG|*.png"
+        fs.Title = "Save to"
+        fs.ShowDialog()
+
+
+        If fs.FileName <> "" Then
+            saveFileName = fs.FileName
+        End If
+
+        SaveT.Text = saveFileName
+    End Sub
+
+    Private Sub OpenT_TextChanged(sender As System.Object, e As System.EventArgs) Handles OpenT.TextChanged
+        fromFileName = OpenT.Text
+    End Sub
+
+    Private Sub SaveT_TextChanged(sender As System.Object, e As System.EventArgs) Handles SaveT.TextChanged
+        saveFileName = SaveT.Text
+    End Sub
+
+End Class
+
